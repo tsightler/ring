ca82e64edbfa0b894709b47cb00a6b68297b0654
tsightler <tsightler@gmail.com>
2026-05-24 23:16:35 -0400

    Use OAuth + PCKE authentication flow

--- packages/ring-client-api/ring-types.ts
+++ packages/ring-client-api/ring-types.ts
@@ -1174,6 +1174,12 @@ export type Auth2faResponse =
       tsv_state: TwoStageVerificationState
     }
 
+export interface Auth2faVerifyResponse {
+  redirect_url: string
+  status: 'auth-completed'
+  user_id: number
+}
+
 export interface ProfileResponse {
   profile: {
     id: number
